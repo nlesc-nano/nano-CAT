2e1a7540168070cb338b305c7bf765d1a6551512
Bas van Beek <b.f.van.beek@vu.nl>
2020-02-28 14:23:31 +0100

    Fixed incorrect module/class names

--- nanoCAT/ff/__init__.py
+++ nanoCAT/ff/__init__.py
@@ -6,13 +6,13 @@ Forcefield-related modules.
 
 """
 
-from .match_job import MATCHJob
-from .ff_assigmnet import run_match_job
+from .match_job import MatchJob
+from .ff_assignment import run_match_job
 from .ff_anionic import run_ff_anionic
 from .ff_cationic import run_ff_cationic
 
 __all__ = [
-    'MATCHJob',
+    'MatchJob',
     'run_match_job',
     'run_ff_anionic',
     'run_ff_cationic'

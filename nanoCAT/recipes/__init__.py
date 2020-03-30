0e6f90b833c42d3271e8aa6c51e37c3148c7fb1c
Bas van Beek <b.f.van.beek@vu.nl>
2020-03-30 17:52:51 +0200

    Added 'coordination_number' the main recipes __init__

--- nanoCAT/recipes/__init__.py
+++ nanoCAT/recipes/__init__.py
@@ -11,6 +11,7 @@ Examples
     >>> from nanoCAT.recipes import bulk_workflow
     >>> from nanoCAT.recipes import get_lig_charge
     >>> from nanoCAT.recipes import replace_surface
+    >>> from nanoCAT.recipes import coordination_number
     >>> from nanoCAT.recipes import dissociate_surface, row_accumulator
     ...
 
@@ -20,8 +21,9 @@ from .bulk import bulk_workflow
 from .charges import get_lig_charge
 from .mark_surface import replace_surface
 from .surface_dissociation import dissociate_surface, row_accumulator
+from .coordination_number import coordination_number
 
 __all__ = [
     'bulk_workflow', 'replace_surface', 'dissociate_surface',
-    'row_accumulator', 'get_lig_charge'
+    'row_accumulator', 'get_lig_charge', 'coordination_number'
 ]

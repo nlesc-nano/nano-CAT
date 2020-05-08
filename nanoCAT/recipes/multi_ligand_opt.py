21b52b537974fdf9f537f3143881052375bf3c58
Bas van Beek <b.f.van.beek@vu.nl>
2020-05-08 20:21:26 +0200

    The new recipe is now more or less functional

--- nanoCAT/recipes/multi_ligand_opt.py
+++ nanoCAT/recipes/multi_ligand_opt.py
@@ -1,8 +1,5 @@
 import os
 from os.path import join
-from enum import IntEnum
-from functools import partial
-from contextlib import nullcontext
 from typing import (
     Union,
     Type,
@@ -15,10 +12,14 @@ from typing import (
     Iterable
 )
 
-from scm.plams import Settings, Molecule, SingleJob, readpdb, MoleculeError, config
+import numpy as np
+import pandas as pd
+
+from scm.plams import Settings, Molecule, SingleJob, writepdb, MoleculeError, config
 from qmflows import run, cp2k_mm, PackageWrapper, Settings as QmSettings
 from qmflows.utils import InitRestart
-from qmflows.packages import Package
+from qmflows.packages import Package, registry
+from noodles.run.threading.sqlite3 import run_parallel
 
 from nanoCAT.ff import MatchJob
 from FOX import PSFContainer, PRMContainer, group_by_values
@@ -29,9 +30,8 @@ PathType = Union[str, os.PathLike]
 __all__ = ['multi_ligand_opt']
 
 
-def multi_ligand_opt(pdb_file: PathType,
+def multi_ligand_opt(mol: Molecule,
                      settings: MutableMapping,
-                     job_type: Union[Type[SingleJob], Package] = cp2k_mm,
                      path: Optional[PathType] = None,
                      folder: Optional[PathType] = None,
                      **kwargs: Any):
@@ -39,33 +39,33 @@ def multi_ligand_opt(pdb_file: PathType,
 
     Examples
     --------
-    .. code:; python
+    .. code:: python
 
         >>> from qmflows.templates import geometry
-        >>> from scm.plams import Settings
+        >>> from qmflows.packages import Result
+        >>> from scm.plams import Molecule, readpdb
+
         >>> from CAT.recipes import multi_ligand_opt
 
-        >>> pdb = str(...)
+        >>> mol: Molecule = readpdb(...)
 
-        >>> settings = Settings()
-        >>> s.specific.cp2k += geometry.specific.cp2k_mm
+        >>> settings = Settings(...)
+        >>> settings.specific.cp2k += geometry.specific.cp2k_mm
 
-        >>> results = multi_ligand_opt(pdb, settings)
-
+        >>> results: Result = multi_ligand_opt(mol, settings)
 
     """
-    job_style, context, runner, s = _parse_job(settings, job_type, path=path, folder=folder)
-    mol = readpdb(pdb_file)
+    _path = os.getcwd() if path is None else os.path.abspath(path)
+    _folder = 'plams_workdir' if folder is None else folder
+    workdir = join(_path, _folder)
+    db_file = join(workdir, 'cache.db')
 
-    with context:
-        ff = kwargs.pop('forcefield', 'top_all36_cgenff_new')
+    s = QmSettings(settings)
+    ff = kwargs.pop('forcefield', 'top_all36_cgenff_new')
 
-        if job_style == _JobStyle.PLAMS:
-            job = job_type(molecule=mol, settings=s, **kwargs)
-            return runner(job)
-
+    with InitRestart(path=path, folder=folder):
         lig_dict = _lig_from_pdb(mol)
-        rtf_list, prm = _run_match(lig_dict.values(), runner, forcefield=ff)
+        rtf_list, prm = _run_match(lig_dict.values(), workdir, forcefield=ff)
 
         psf = PSFContainer()
         psf.generate_bonds(mol)
@@ -73,44 +73,42 @@ def multi_ligand_opt(pdb_file: PathType,
         psf.generate_dihedrals(mol)
         psf.generate_impropers(mol)
         psf.generate_atoms(mol)
-        for id_range, rtf_file in zip(lig_dict.keys(), rtf_list):
+
+        start = int(psf.segment_name.iloc[-1].strip('MOL'))
+        iterator = enumerate(zip(lig_dict.keys(), rtf_list), start=start)
+        for i, (id_range, rtf_file) in iterator:
             overlay_rtf_file(psf, rtf_file, id_range)
+            idx = np.zeros_like(psf.residue_id, dtype=bool)
+            for j in id_range:
+                idx[psf.residue_id == j] = True
+            psf.atoms.loc[idx, 'segment name'] = f'MOL{i}'
 
-        workdir = config.default_jobmanager.workdir
-        psf.write(join(workdir, 'mol.psf'))
-        prm.write(join(workdir, 'mol.prm'))
-        s.psf = join(workdir, 'mol.psf')
-        s.prm = join(workdir, 'mol.prm')
+        prm_name = join(workdir, 'mol.prm')
+        psf_name = join(workdir, 'mol.psf')
+        prm.write(prm_name)
+        psf.write(psf_name)
+        s.prm = prm_name
+        s.psf = psf_name
 
-        job = job_type(mol=mol, settings=s, **kwargs)
-        return runner(job)
-
-
-class _JobStyle(IntEnum):
-    PLAMS = 0
-    QMFLOWS = 1
+        config.log.stdout = 0
+        job = cp2k_mm(mol=mol, settings=s, **kwargs)
+        return run_parallel(job, db_file=db_file, n_threads=1, always_cache=True,
+                            registry=registry, echo_log=False)
 
 
-def _run_match(mol_list: Iterable[Molecule], runner=None,
+def _run_match(mol_list: Iterable[Molecule], workdir,
                forcefield: str = 'top_all36_cgenff_new') -> Tuple[List[str], PRMContainer]:
     s = Settings()
     s.input.forcefield = forcefield
 
-    if runner is not None:
-        runner_ = runner
-        partial_job = partial(PackageWrapper(MatchJob), settings=s)
-    else:
-        runner_ = MatchJob.run
-        partial_job = partial(MatchJob, settings=s)
-
     rtf_list = []
     prm_list = []
-    for mol in mol_list:
-        job = partial_job(molecule=mol) if runner is None else partial_job(mol=mol)
-        results = runner_(job)
+    for i, mol in enumerate(mol_list):
+        job = MatchJob(molecule=mol, settings=s, name=f'match_job{i}')
+        results = job.run()
 
-        rtf = results.files['$JN.rtf']
-        prm = results.files['$JN.prm']
+        rtf = results['$JN.rtf']
+        prm = results['$JN.prm']
         rtf_list.append(rtf)
         prm_list.append(prm)
 
@@ -124,7 +122,7 @@ def _concatenate_prm(file_list: Iterable[PathType]) -> PRMContainer:
     ret = next(iterator)
 
     for prm in iterator:
-        items = ((k, getattr(prm, k)) for k in prm.HEADERS[:-2])
+        items = ((k.lower(), getattr(prm, k.lower())) for k in prm.HEADERS[:-2])
         for k, v in items:
             df = getattr(ret, k)
             if v is None:
@@ -134,8 +132,10 @@ def _concatenate_prm(file_list: Iterable[PathType]) -> PRMContainer:
             else:
                 setattr(ret, k, df.append(v))
 
-    items = ((k, getattr(prm, k)) for k in prm.HEADERS[:-2])
+    items = ((k.lower(), getattr(ret, k.lower())) for k in ret.HEADERS[:-2])
     for k, df in items:
+        if df is None:
+            continue
         df_ = df.loc[~df.index.duplicated(keep='first')]
         setattr(ret, k, df)
     return ret
@@ -162,22 +162,3 @@ def _lig_from_pdb(mol: Molecule) -> Dict[Tuple[int, ...], Molecule]:
         i = v[-1] - 2
         ret[k] = ligands[i]
     return ret
-
-
-def _parse_job(settings, job_type, path=None, folder=None):
-    if isinstance(job_type, Package):
-        job_style = _JobStyle.QMFLOWS
-        context = nullcontext()
-        s = QmSettings(settings) if type(settings) is not QmSettings else settings
-        runner = partial(run, path=path, folder=folder)
-
-    elif isinstance(job_type, type) and issubclass(job_type, SingleJob):
-        job_style = _JobStyle.PLAMS
-        context = InitRestart(path=path, folder=folder)
-        s = Settings(settings) if type(settings) is not Settings else settings
-        runner = type(job_type).run
-
-    else:
-        raise TypeError("'job_type' expected a Job subclass or Package instance; "
-                        f"observed type: {job_type.__class__.__name__!r}")
-    return job_style, context, runner, s

bc2cf10cd4638d4e411a874f66dc0ca2b1dee914
Bas van Beek <b.f.van.beek@vu.nl>
2021-04-16 16:38:52 +0200

    ENH: Add the initial implementation of a new (faster) bulkiness workflow

---
+++ nanoCAT/bulk/__init__.py
@@ -0,0 +1,8 @@
+"""A module with various bulkiness related workflows."""
+
+# flake8: noqa: E402
+
+from . import mol_graph
+from mol_graph import GraphConstructor, NeighborTuple, yield_distances
+
+__all__ = mol_graph.__all__.copy()

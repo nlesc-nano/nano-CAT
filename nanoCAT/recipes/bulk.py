dbe3cf6f51b074643348f38e418e807d48bbef73
Bas van Beek <b.f.van.beek@vu.nl>
2021-06-03 00:02:05 +0200

    STY: Miscellaneous style improvements

--- nanoCAT/recipes/bulk.py
+++ nanoCAT/recipes/bulk.py
@@ -16,7 +16,9 @@ API
 
 """
 
-from typing import Iterable, List, Iterator, Callable, Generator, Tuple, Optional
+from __future__ import annotations
+
+from typing import Iterable, List, Iterator, Callable, Generator, Tuple, TYPE_CHECKING
 from itertools import chain
 
 import numpy as np
@@ -26,18 +28,22 @@ from CAT.data_handling.mol_import import read_mol
 from CAT.data_handling.validate_mol import validate_mol
 from CAT.attachment.ligand_anchoring import _smiles_to_rdmol, find_substructure
 from CAT.attachment.ligand_opt import optimize_ligand, allign_axis
-
 from nanoCAT.mol_bulk import get_V, get_lig_radius
+
+if TYPE_CHECKING:
+    import numpy.typing as npt
 
 __all__ = ['bulk_workflow']
 
 
-def bulk_workflow(smiles_list: Iterable[str],
-                  anchor: str = 'O(C=O)[H]',
-                  anchor_condition: Optional[Callable[[int], bool]] = None,
-                  diameter: Optional[float] = 4.5,
-                  height_lim: Optional[float] = 10.0,
-                  optimize: bool = True) -> Tuple[List[Molecule], np.ndarray]:
+def bulk_workflow(
+    smiles_list: Iterable[str],
+    anchor: str = 'O(C=O)[H]',
+    anchor_condition: None | Callable[[int], bool] = None,
+    diameter: None | float = 4.5,
+    height_lim: None | float = 10.0,
+    optimize: bool = True,
+) -> Tuple[List[Molecule], npt.NDArray[np.float64]]:
     """Start the CAT ligand bulkiness workflow with an iterable of smiles strings.
 
     Examples
@@ -52,38 +58,33 @@ def bulk_workflow(smiles_list: Iterable[str],
 
     Parameters
     ----------
-    smiles_list : :class:`~collections.abc.Iterable` [:class:`str`]
+    smiles_list : :class:`Iterable[str] <collections.abc.Iterable>`
         An iterable of SMILES strings.
-
     anchor : :class:`str`
         A SMILES string representation of an anchor group such as ``"O(C=O)[H]"``.
         The first atom will be marked as anchor atom while the last will be dissociated.
         Used for filtering molecules in **smiles_list**.
-
-    anchor_condition : :class:`Callable[[int], bool]<collections.abc.Callable>`, optional
+    anchor_condition : :class:`Callable[[int], bool] <collections.abc.Callable>`, optional
         If not :data:`None`, filter ligands based on the number of identified functional groups.
         For example, ``anchor_condition = lambda n: n == 1`` will only accept ligands with
         a single **anchor** group,
         ``anchor_condition = lambda n: n >= 3`` requires three or more anchors and
         ``anchor_condition = lambda n: n < 2`` requires fewer than two anchors.
-
     diameter : :class:`float`, optional
         The lattice spacing, *i.e.* the average nearest-neighbor distance between the anchor atoms
         of all ligads.
         Set to :data:`None` to ignore the lattice spacing.
         Units should be in Angstrom.
-
     height_lim : :class:`float`, optional
         A cutoff above which all atoms are ignored.
         Set to :data:`None` to ignore the height cutoff.
         Units should be in Angstrom.
-
     optimize : :class:`bool`
         Enable or disable the ligand geometry optimization.
 
     Returns
     -------
-    :class:`list` [:class:`Molecule<scm.plams.mol.molecule.Molecule>`] & :class:`numpy.ndarray`
+    :class:`list[plams.Molecule] <list>` and :class:`numpy.ndarray`
         A list of plams Molecules and a matching array of :math:`V_{bulk}` values.
 
     """
@@ -103,9 +104,11 @@ def read_smiles(smiles_list: Iterable[str]) -> List[Molecule]:
     return read_mol(input_mol)
 
 
-def _filter_mol(mol_list: Iterable[Molecule],
-                anchor: str = 'O(C=O)[H]',
-                condition: Optional[Callable[[int], bool]] = None) -> Iterator[Molecule]:
+def _filter_mol(
+    mol_list: Iterable[Molecule],
+    anchor: str = 'O(C=O)[H]',
+    condition: None | Callable[[int], bool] = None,
+) -> Iterator[Molecule]:
     """Filter all input molecules based on the presence of a functional group (the "anchor")."""
     anchor_rdmols = (_smiles_to_rdmol(anchor),)
     return chain.from_iterable(
@@ -123,8 +126,11 @@ def opt_and_allign(mol_list: Iterable[Molecule], opt: bool = True) -> None:
         process_mol(mol)
 
 
-def bulkiness(mol_list: Iterable[Molecule], diameter: Optional[float] = 4.5,
-              height_lim: Optional[float] = 10.0) -> np.ndarray:
+def bulkiness(
+    mol_list: Iterable[Molecule],
+    diameter: None | float = 4.5,
+    height_lim: None | float = 10.0,
+) -> npt.NDArray[np.float64]:
     r"""Calculate the ligand bulkiness descriptor :math:`V_{bulk}`.
 
     .. math::
@@ -142,4 +148,4 @@ def bulkiness(mol_list: Iterable[Molecule], diameter: Optional[float] = 4.5,
         count = len(mol_list)  # type: ignore
     except TypeError:
         count = -1
-    return np.fromiter(_iterate(mol_list), count=count, dtype=float)
+    return np.fromiter(_iterate(mol_list), count=count, dtype=np.float64)

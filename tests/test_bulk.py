413abd316f3dd770cbb383723efa0e57b37824bd
Bas van Beek <b.f.van.beek@vu.nl>
2021-06-08 09:16:54 +0200

    TST: Update the `fast_bulk_workflow` tests

--- tests/test_bulk.py
+++ tests/test_bulk.py
@@ -2,24 +2,18 @@
 
 from __future__ import annotations
 
+from pathlib import Path
 from typing import Mapping, Any
 
 import numpy as np
+import pandas as pd
 import pytest
 from assertionlib import assertion
 from nanoCAT.recipes import bulk_workflow, fast_bulk_workflow
 
-FAST_BULK_REF = np.array([
-    [40.16235, 207.024513, 987.120114],
-    [41.16235, 208.024513, 988.120114],
-    [8.852858, 21.181752, 38.191051],
-    [0.0, 0.0, 91.447878],
-    [0.0, 0.0, 0.54337059],
-    [0.0, 0.0, 749.750132],
-    [0.0, 0.0, 749.750132],
-    [0.0, 0.0, 749.750132],
-], dtype=np.float64)
-FAST_BULK_REF.setflags(write=False)
+PATH = Path("tests") / "test_files"
+FAST_BULK_REF = pd.read_csv(PATH / "bulk.csv", index_col=0)
+FAST_BULK_REF.columns = FAST_BULK_REF.columns.astype(np.int64)
 
 
 def test_bulk_workflow() -> None:
@@ -37,24 +31,20 @@ def test_bulk_workflow() -> None:
     np.testing.assert_allclose(bulk_ar, ref)
 
 
-@pytest.mark.parametrize("kwargs,i", [
-    (dict(diameter=1), 0),
-    (dict(diameter=None), 1),
-    (dict(func=None), 2),
-    (dict(func=lambda x: x**2), 3),
-    (dict(func=lambda x: 1/x), 4),
-    (dict(height_lim=5), 5),
-    (dict(height_lim=None), 6),
-    ({}, 7),
+@pytest.mark.parametrize("kwargs,i,j", [
+    (dict(diameter=1), 0, 15),
+    (dict(diameter=None), 1, 15),
+    (dict(func=None), 2, 15),
+    (dict(func=lambda x: x**2), 3, 15),
+    (dict(func=lambda x: 1/x), 4, 15),
+    (dict(height_lim=5), 5, 15),
+    (dict(height_lim=None), 6, 15),
+    ({}, 7, None),
 ])
-def test_fast_bulk_workflow(kwargs: Mapping[str, Any], i: int) -> None:
+def test_fast_bulk_workflow(kwargs: Mapping[str, Any], i: int, j: None | int) -> None:
     """Tests for :func:`fast_bulk_workflow`."""
-    smiles_list = ['CO', 'CCO', 'CCCO']
-    mol_list, bulk_ar = fast_bulk_workflow(smiles_list, anchor='O[H]', **kwargs)
+    smiles_list = FAST_BULK_REF.index[:j]
+    _, bulk_ar = fast_bulk_workflow(smiles_list, **kwargs)
 
-    formula_list = ['C1H3O1', 'C2H5O1', 'C3H7O1']
-    for mol, formula in zip(mol_list, formula_list):
-        assertion.eq(mol.get_formula(), formula)
-
-    ref = FAST_BULK_REF[i]
-    np.testing.assert_allclose(bulk_ar, ref, rtol=1e-07)
+    ref = FAST_BULK_REF.iloc[:j, i]
+    np.testing.assert_allclose(bulk_ar, ref)
